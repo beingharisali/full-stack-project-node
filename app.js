a8bd38b9ebbbb08df70988108124333424aca0d5
beingharisali <theharis99@gmail.com>
2025-09-02 15:13:21 +0500

    updated frontend url

--- app.js
+++ app.js
@@ -3,12 +3,14 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const connectDB = require("./database/connect");
 const taskRouter = require("./routes/task");
 const port = process.env.PORT;
 
 connectDB();
 app.use(express.json());
+app.use(cors({ origin: process.env.FRONTEND_URL }));
 app.use("/", taskRouter);
 
 app.listen(port, () => {
